c1aee10f17d56f460e7a1b12001b8867bfd3e206
Sofie Andersson <sofiesofieandersson@gmail.com>
2024-10-29 21:52:25 +0100

    first commit og alt indhold ind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        lilla: "#9b6496",
+        gron: "#7DAB81",
+      },
+      fontSize: {
+        header: "5.2rem",
+      },
+      fontFamily: {
+        oswald: ["oswald", "sans-serif"],
+        mont: ["mont", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
